4173759ad5668ad8d7593bc78038e739992d24d2
Emanuel Vieira <emanuelvisantos99@gmail.com>
2021-09-28 20:40:34 -0300

    Adding api url environment variable

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './globalStyle/index.css';
 import App from './App';
 import axios from 'axios';
-axios.defaults.baseURL = process.env.REACT_APP_API_URL
+axios.defaults.baseURL = process.env.PROCAD_API_URL || process.env.REACT_APP_API_URL
 
 ReactDOM.render(
   <React.StrictMode>
